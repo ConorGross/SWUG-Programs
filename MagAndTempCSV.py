be566be16839bd0c3fc89496998f439487f9e1c6
ConorGross <grossc@cua.edu>
2023-06-27 13:18:56 -0400

    Adding MagAndTempCSV file to repository

---
+++ MagAndTempCSV.py
@@ -0,0 +1,31 @@
+import serial # import serial module
+import csv
+
+ser = serial.Serial("COM4", 9600) # assign variable 'ser' with magnetometer kit COM4
+ser2 = serial.Serial("COM6", 9600) # assign variable 'ser2' with tempersture sensor COM6
+count = 0 # initialize counter
+with open('Data.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Date", "Time", "Mag X", "Mag Y", "Mag Z", "Temp Sensor"])
+
+while ser.in_waiting >= 0 and count < 20 and ser2.in_waiting >=0: # run a while loop
+
+    mag=str(ser.readline().decode("utf-8")) # convert magnetometer reading to string
+    print(mag)
+    
+    temp=str(ser2.readline().decode("utf-8")) # convert temperature sensor reading to string
+    print(temp) 
+
+    x = mag.find('X')
+    y = mag.find(',Y')
+    z = mag.find(',Z')
+        
+    with open('Data.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([mag[0:9], mag[10:18], mag[x+2:y], mag[y+3:z], mag[z+3:], temp[13:18]])
+    
+    count = count + 1 
+
+
+ser.close() # close port COM4
+ser2.close() # close port COM6
