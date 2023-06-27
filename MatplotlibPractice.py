9ef6d35d5a44463decdde874c0735b489b30c978
ConorGross <grossc@cua.edu>
2023-06-27 16:34:36 -0400

    Added practice file for Matplotlib to look at for reference

---
+++ MatplotlibPractice.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+
+
+fig, ax = plt.subplots()
+
+twin1 = ax.twinx() #create twin axes for second data set
+
+# Offset the right spine of twin2.  The ticks and label have already been
+# placed on the right by twinx above.
+
+
+p1, = ax.plot([0, 1, 2], [0, 1, 2], "b-", label="Density")
+p2, = twin1.plot([0, 1, 2], [0, 3, 2], "r-", label="Temperature")
+
+
+ax.set_xlim(0, 2)
+ax.set_ylim(0, 2)
+twin1.set_ylim(0, 4)
+
+
+ax.set_xlabel("Distance")
+ax.set_ylabel("Density")
+twin1.set_ylabel("Temperature")
+
+
+ax.yaxis.label.set_color(p1.get_color())
+twin1.yaxis.label.set_color(p2.get_color())
+
+
+tkw = dict(size=4, width=1.5)
+ax.tick_params(axis='y', colors=p1.get_color(), **tkw)
+twin1.tick_params(axis='y', colors=p2.get_color(), **tkw)
+ax.tick_params(axis='x', **tkw)
+
+ax.legend(handles=[p1, p2])
+
+plt.show()
