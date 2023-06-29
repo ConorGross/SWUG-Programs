063fa52806499ac0c1eb3ab47683fd00bc41bc23
ConorGross <grossc@cua.edu>
2023-06-29 16:05:14 -0400

    Got a grpah working, need to work on x-axis marks

--- MatplotlibPractice.py
+++ MatplotlibPractice.py
@@ -1,18 +1,31 @@
 import matplotlib.pyplot as plt
-import csv
 import pandas as pd
+import matplotlib.ticker as ticker
 
-#going to use dataframe.tail() function to find and insert last 20 lines of data for graph
-#maxSensorX = df['Mag X'].max()
-#maxSensorY = df['Mag Y'].max()
-#maxSensorZ = df['Mag Z'].max()
-#minSensorX = df['Mag X'].min()
-#minSensorY = df['Mag Y'].min()
-#minSensorZ = df['Mag Z'].min()
-#maxTemp = df['Temp Sensor'].max()
-#minTemp = df['Temp Sensor'].min()
-#maxTime = df['Time'].max()
-#minTime = df['Time'].min()
+
+df = pd.read_csv("Data.csv") #convert CSV file to dataframe
+liveData = df.tail(20) #only pull most recent data
+
+def getMaxSensor(SensorMax):
+    liveData[SensorMax].max()
+    
+
+def getMinSensor(SensorMin):
+    liveData[SensorMin].min()
+    
+
+maxMagX = getMaxSensor('MagX') #get max values
+maxMagY = getMaxSensor('MagY')
+maxMagZ = getMaxSensor('MagZ')
+maxTemp = getMaxSensor('TempSensor')
+maxTime = getMaxSensor('Time')
+
+minMagX = getMinSensor('MagX') #get min values
+minMagY = getMinSensor('MagY')
+minMagZ = getMinSensor('MagZ')
+minTemp = getMinSensor('TempSensor')
+minTime = getMinSensor('Time')
+
 
 fig, ax = plt.subplots()
 
@@ -22,12 +35,12 @@ twin1 = ax.twinx() #create twin axes for second data set
 # placed on the right by twinx above.
 
 
-p1, = ax.plot(mag[10:18], mag[x+2:y], "b-", label="Sensor X") #plot first data set
-p2, = twin1.plot(mag[10:18], temp[13:18], "r-", label="Temperature") #plot second data set
+p1, = ax.plot(liveData['Time'], liveData['MagX'], "b-", label="Sensor X") #plot first data set
+p2, = twin1.plot(liveData['Time'], liveData['TempSensor'], "r-", label="Temperature") #plot second data set
 
 
 ax.set_xlim(minTime, maxTime) #set limits for axis x
-ax.set_ylim(minSensorX, maxSensorX) #set limits for data 1 y
+ax.set_ylim(minMagX, maxMagX) #set limits for data 1 y
 twin1.set_ylim(minTemp, maxTemp) #set limits for data 2 y
 
 
@@ -39,11 +52,14 @@ twin1.set_ylabel("Temperature")
 ax.yaxis.label.set_color(p1.get_color())
 twin1.yaxis.label.set_color(p2.get_color())
 
+tkw = dict(size=4, width=0.5)
 
-tkw = dict(size=4, width=1.5)
 ax.tick_params(axis='y', colors=p1.get_color(), **tkw)
 twin1.tick_params(axis='y', colors=p2.get_color(), **tkw)
 ax.tick_params(axis='x', **tkw)
+def setup(ax, title="LinearLocator(numticks=5)"):
+    ax.xaxis.set_major_locator(ticker.LinearLocator(5))
+    ax.xaxis.set_minor_locator(ticker.LinearLocator(20))
 
 ax.legend(handles=[p1, p2])
 
