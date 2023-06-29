de4931156f81b5333cc017183151db2497cff832
ConorGross <grossc@cua.edu>
2023-06-29 16:05:50 -0400

    Code is still a work in progress

--- MagAndTempCSV.py
+++ MagAndTempCSV.py
@@ -1,12 +1,12 @@
-import serial # import serial module
+import serial
 import csv
+import pandas as pd
+import matplotlib.pyplot as plt
 
 ser = serial.Serial("COM4", 9600) # assign variable 'ser' with magnetometer kit COM4
 ser2 = serial.Serial("COM6", 9600) # assign variable 'ser2' with tempersture sensor COM6
 count = 0 # initialize counter
-with open('Data.csv', 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["Date", "Time", "Mag X", "Mag Y", "Mag Z", "Temp Sensor"])
+
 
 while ser.in_waiting >= 0 and count < 20 and ser2.in_waiting >=0: # run a while loop
 
@@ -22,10 +22,12 @@ while ser.in_waiting >= 0 and count < 20 and ser2.in_waiting >=0: # run a while 
         
     with open('Data.csv', 'a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([mag[0:9], mag[10:18], mag[x+2:y], mag[y+3:z], mag[z+3:], temp[13:18]])
+        writer.writerow([mag[0:9], mag[10:18], mag[x+2:y], mag[y+3:z], mag[z+3:], temp[13:18]]) #add new data to Data file
     
     count = count + 1 
 
 
+
 ser.close() # close port COM4
 ser2.close() # close port COM6
+
