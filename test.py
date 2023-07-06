5c5f8ef179803a33d7c2295ea2dcf68b41523aa5
ConorGross <grossc@cua.edu>
2023-07-06 12:06:03 -0400

    Trying to figure out why code won't run

--- test.py
+++ test.py
@@ -1,26 +1,129 @@
-import numpy as np
+import serial
+import csv
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+from os import path
+
+if path.exists('Data.csv'):
+        with open ('Data.csv') as file:
+            reader = csv.reader(file)
+            header = []
+            for row in file:
+                header = row
+                break
+                    
+        if header == ["Date", "Time", "MagX", "MagY", "MagZ", "TempSensor"]:
+        
+            date = []
+            time = []
+            magX = []
+            magY = []
+            magZ = []
+            tempSensor = []
+
+            ser = serial.Serial("COM4", 9600) # assign variable 'ser' with magnetometer kit COM4
+            ser2 = serial.Serial("COM6", 9600) # assign variable 'ser2' with tempersture sensor COM6
+            runTime = 0
+            while ser.in_waiting >= 0 and runTime < 20 and ser2.in_waiting >=0: # run a while loop
+
+                    mag=str(ser.readline().decode("utf-8")) # convert magnetometer reading to string
+                    print(mag)
+                    
+                    temp=str(ser2.readline().decode("utf-8")) # convert temperature sensor reading to string
+                    print(temp) 
+
+                    x = mag.find('X')
+                    y = mag.find(',Y')
+                    z = mag.find(',Z')
+                    t = temp.find('C')
+
+                    date.append(mag[0:9])
+                    time.append(mag[10:18])
+                    magX.append(mag[x+2:y])
+                    magY.append(mag[y+3:z])
+                    magZ.append(mag[z+3:])
+                    tempSensor.append(temp[t-6:t-1])
+
+                    runTime = runTime + 1 
+
+            with open('Data.csv', 'a', newline='') as file:
+                    writer = csv.writer(file)
+                    count = 0
+                    while count < len(date):
+                        writer.writerow([date[count], time[count], magX[count], magY[count], magZ[count], tempSensor[count]])
+                        count = count + 1
+                    
+                    file.close
+
+            ser.close() # close port COM4
+            ser2.close() # close port COM6
+
+                
+            def getMaxSensor(liveData, SensorMax):
+                liveData[SensorMax].max()
+                        
+
+            def getMinSensor(liveData, SensorMin):
+                liveData[SensorMin].min()
+
+            
+            df = pd.read_csv("Data.csv") #convert CSV file to dataframe
+            liveData = df.tail() #only pull most recent data
+
+                        
+
+            maxMagX = getMaxSensor(liveData, 'MagX') #get max values
+            maxMagY = getMaxSensor(liveData, 'MagY')
+            maxMagZ = getMaxSensor(liveData, 'MagZ')
+            maxTemp = getMaxSensor(liveData, 'TempSensor')
+            maxTime = getMaxSensor(liveData, 'Time')
+
+            minMagX = getMinSensor(liveData, 'MagX') #get min values
+            minMagY = getMinSensor(liveData, 'MagY')
+            minMagZ = getMinSensor(liveData, 'MagZ')
+            minTemp = getMinSensor(liveData, 'TempSensor')
+            minTime = getMinSensor(liveData, 'Time')
 
 
-def setup(ax, title="LinearLocator(numticks=3)"):
-    """Set up common parameters for the Axes in the example."""
-    # only show the bottom spine
-    ax.yaxis.set_major_locator(ticker.NullLocator())
-    ax.spines.right.set_color('none')
-    ax.spines.left.set_color('none')
-    ax.spines.top.set_color('none')
+            fig, ax = plt.subplots()
 
-    ax.xaxis.set_ticks_position('bottom')
-    ax.tick_params(which='major', width=1.00, length=5)
-    ax.tick_params(which='minor', width=0.75, length=2.5)
-    ax.set_xlim(0, 5)
-    ax.set_ylim(0, 1)
-    ax.text(0.0, 0.2, title, transform=ax.transAxes,
-            fontsize=14, fontname='Monospace', color='tab:blue')
+            twin1 = ax.twinx() #create twin axes for second data set
 
-fig, axs = plt.subplots(8, 1, figsize=(8, 6))
+            p1, = ax.plot(liveData['Time'], liveData['MagZ'], "b-", label="Sensor X") #plot first data set
+            p2, = twin1.plot(liveData['Time'], liveData['TempSensor'], "r-", label="Temperature") #plot second data set
 
-setup(axs[3], title="LinearLocator(numticks=3)")
-axs[3].xaxis.set_major_locator(ticker.LinearLocator(3))
-axs[3].xaxis.set_minor_locator(ticker.LinearLocator(31))
+
+            ax.set_xlim(minTime, maxTime) #set limits for axis x
+            ax.set_ylim(minMagZ, maxMagZ) #set limits for data 1 y
+            twin1.set_ylim(minTemp, maxTemp) #set limits for data 2 
+
+
+            ax.set_xlabel("Time")
+            ax.set_ylabel("Sensor Z")
+            twin1.set_ylabel("Temperature")
+
+
+            ax.yaxis.label.set_color(p1.get_color())
+            twin1.yaxis.label.set_color(p2.get_color())
+
+            tkw = dict(size=4, width=0.5)
+
+            ax.tick_params(axis='y', colors=p1.get_color(), **tkw)
+            twin1.tick_params(axis='y', colors=p2.get_color(), **tkw)
+            ax.tick_params(axis='x', **tkw)
+            ax.xaxis.set_major_locator(ticker.LinearLocator(5))
+            ax.xaxis.set_minor_locator(ticker.LinearLocator(21))
+
+
+            ax.legend(handles=[p1, p2])
+
+            plt.show()
+        else:
+            with open('Data.csv', 'w') as file:
+                writer = csv.writer(file)
+                writer.writerow(["Date", "Time", "MagX", "MagY", "MagZ", "TempSensor"])
+else:
+    with open('Data.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Date", "Time", "MagX", "MagY", "MagZ", "TempSensor"])
